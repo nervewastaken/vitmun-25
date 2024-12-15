adbda56d0b994c8a747e73fb55e02a38536f2d09
nervewastaken <krishverma2004@gmail.com>
2024-12-15 19:47:36 +0530

    added redirect functionality on the home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,7 @@ import { NextURL } from "next/dist/server/web/next-url";
 
 export default clerkMiddleware((req) => {
   const url = new NextURL(req.url, { base: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000" });
-  const pathname = url.pathname; // Get the pathname from the NextURL instance
 
-  // Apply authentication check for `/admin` and `/api/admin` routes
-  if (pathname.startsWith("/admin") || pathname.startsWith("/api/admin")) {
-    const { userId } = req.auth;
-
-    // If the user is not authenticated, return a 401 Unauthorized response
-    if (!userId) {
-      return NextResponse.json({ error: "Bad auth!" }, { status: 401 });
-    }
-  }
 
   return NextResponse.next(); // Proceed if authenticated
 });
