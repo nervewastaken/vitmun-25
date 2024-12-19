935d8fa20f3278e529242f3b1a141ce688935b98
nervewastaken <krishverma2004@gmail.com>
2024-12-19 16:52:10 +0530

    deployment hotfix 2
    I did remove the error check for duplicate data in /src/app/registrations/delegation adding that as issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,21 +6,11 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { Roboto } from "next/font/google"
+import { Roboto } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+//fonts deleted
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -43,13 +33,14 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning className={`${roboto.variable}`}>
         <body>
           <SignedOut>
-             {/* No redirectUrl here; redirect handled globally */}
+            <SignInButton />{" "}
+            {/* No redirectUrl here; redirect handled globally */}
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
           {children}
-          <Toaster/>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
