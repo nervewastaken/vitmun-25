269d05506dc9b896ebfc432c25e22f269c45e38e
nervewastaken <krishverma2004@gmail.com>
2024-12-19 17:04:57 +0530

    deployment hotfix 3.

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,34 +1,32 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local");
+  throw new Error("Please add your MongoDB URI to the .env.local file.");
 }
 
-const uri: string = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
 
-let client: MongoClient;
+let client: MongoClient | null = null;
 let clientPromise: Promise<MongoClient>;
-
-// Extend the NodeJS global interface to include _mongoClientPromise
-declare global {
-  namespace NodeJS {
-    interface Global {
-      _mongoClientPromise?: Promise<MongoClient>;
-    }
-  }
-}
 
 // Check the environment
 if (process.env.NODE_ENV === "development") {
-  // Use a global variable to prevent multiple connections in development
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+  // Use a global variable to persist the client instance during hot reloads
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = (global as any)._mongoClientPromise;
 } else {
   // In production, create a new client instance
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
