fcee90b55e69fafa24de72f684fa6603f7760b7e
nervewastaken <krishverma2004@gmail.com>
2024-12-19 17:49:43 +0530

    I dont even wanna talk about it.

--- src/app/api/admin/update-delegate/route.ts
+++ src/app/api/admin/update-delegate/route.ts
@@ -1,9 +1,9 @@
 import { ObjectId } from "mongodb";
 import clientPromise from "../../../../../lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   // Get authentication details
   const { userId } = getAuth(req);
 
