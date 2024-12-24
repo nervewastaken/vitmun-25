2579f69651a0a6819ab6e08f38aafd0bf814188b
nervewastaken <krishverma2004@gmail.com>
2024-12-25 00:41:04 +0530

    deployment hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,16 +10,6 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Roboto } from "next/font/google"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const roboto = Roboto({
   subsets: ["latin"],
