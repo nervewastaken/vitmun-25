3bdbcdb7ec48d76b703b34f841995045f00b6767
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2025-01-01 15:57:42 +0530

    fix: update committee (#19)
    Co-authored-by: Aarjav Jain <aarjavkjain2@gmail.com>

--- src/app/committees/page.tsx
+++ src/app/committees/page.tsx
@@ -5,6 +5,7 @@ import CommitteeCard from "@/components/custom/committee";
 import { Lora } from "next/font/google";
 import { ReactLenis } from "@studio-freight/react-lenis";
 import Navbar from "@/components/custom/Navbar";
+import Image from "next/image";
 
 const lora = Lora({
   subsets: ["latin"],
@@ -13,19 +14,19 @@ const lora = Lora({
 });
 
 const committees = [
-  { name: "AIPPM", image: "/committee/AIPPM.png", agenda: "Agenda for AIPPM", board: "Board for AIPPM" },
-  { name: "CHAOS", image: "/committee/CHAOS.png", agenda: "Agenda for CHAOS", board: "Board for CHAOS" },
-  { name: "DISEC", image: "/committee/DISEC.png", agenda: "Agenda for DISEC", board: "Board for DISEC" },
-  { name: "JHES", image: "/committee/JHES.png", agenda: "Agenda for JHES", board: "Board for JHES" },
-  { name: "ORF", image: "/committee/ORF.png", agenda: "Agenda for ORF", board: "Board for ORF" },
-  { name: "UNGA-SOCHUM", image: "/committee/UNGA-SOCHUM.png", agenda: "Agenda for UNGA-SOCHUM", board: "Board for UNGA-SOCHUM" },
-  { name: "UNSC", image: "/committee/UNSC.png", agenda: "Agenda for UNSC", board: "Board for UNSC" },
+  { name: "AIPPM", image: "/committee/AIPPM.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "CHAOS", image: "/committee/CHAOS.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "DISEC", image: "/committee/DISEC.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "JHES", image: "/committee/JHES.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "ORF", image: "/committee/ORF.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "UNGA-SOCHUM", image: "/committee/UNGA-SOCHUM.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
+  { name: "UNSC", image: "/committee/UNSC.png", agenda: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.", board: { "Chair": "Person A", "Vice Chair": "Person B", "Scribe": "Person C" } },
 ];
 
 const CommitteesPage = () => {
-  const [selectedCommittee, setSelectedCommittee] = useState<{ name: string, agenda: string, board: string } | null>(null);
+  const [selectedCommittee, setSelectedCommittee] = useState<{ name: string, agenda: string, board: { [key: string]: string } } | null>(null);
 
-  const handleCommitteeClick = (committee: { name: string, agenda: string, board: string }) => {
+  const handleCommitteeClick = (committee: { name: string, agenda: string, board: { [key: string]: string } }) => {
     setSelectedCommittee(committee);
   };
 
@@ -54,28 +55,31 @@ const CommitteesPage = () => {
         <p className="text-md md:text-lg font-light mb-6 text-left">
           Presenting the committees for VITMUN'25
         </p>
-        <div className="grid gap-4 md:gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-2 md:gap-4 lg:gap-7 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {committees.map((committee) => (
             <CommitteeCard
               key={committee.name}
               image={committee.image}
               name={committee.name}
               onClick={() => handleCommitteeClick(committee)}
+              isSelected={selectedCommittee?.name === committee.name}
             />
           ))}
         </div>
         {selectedCommittee && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 max-w-3xl mx-auto text-center]">
-              <h2 className="text-xl font-bold mb-4">{selectedCommittee.name}</h2>
-              <p className="mb-4">Agenda : {selectedCommittee.agenda}</p>
-              <p className="mb-4">Board : {selectedCommittee.board}</p>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 bg-blur-md">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 max-w-3xl mx-auto text-center relative">
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="absolute top-4 right-4 h-12"
                 onClick={closeModal}
               >
-                Close
+                <Image src="/cross.svg" alt="Close" width={24} height={24} />
               </button>
+              <h2 className="text-3xl font-bold mb-4">{selectedCommittee.name}</h2>
+              <p className="text-l mb-4"><strong>Agenda:</strong> {selectedCommittee.agenda}</p>
+              {Object.entries(selectedCommittee.board).map(([position, person]) => (
+                <p key={position} className="mb-2"><strong>{position}:</strong> {person}</p>
+              ))}
             </div>
           </div>
         )}
